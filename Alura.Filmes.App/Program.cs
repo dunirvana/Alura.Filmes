c07356dde12d05a6a82c00a6bdd1b08f961a1613
Eduardo Teixeira <dunirvana@gmail.com>
2021-10-28 09:35:39 -0300

    Configurando nomes de tabelas e colunas

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //select * from actor
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                foreach (var ator in contexto.Atores)
+                {
+                    System.Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
